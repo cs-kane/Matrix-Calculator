7b839d96cfa6831dc1f3d5b0a5fef52f81a2b204
TheFool <165768092+cs-kane@users.noreply.github.com>
2024-10-20 02:41:39 +0700

    Added functionalities, design changes

--- matrix_calculator.c
+++ matrix_calculator.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <conio.h>
 
 #define MATRIX_LIMIT 1000
-#define OPERATIONS 5
+#define OPERATIONS 7
 
 void add(int columns, int target_r, int source_r, double factor, double matrix[][columns]);
 void multi(int columns, int row, double factor, double matrix[][columns], int type);
 void swap(int columns, int row1, int row2, double matrix[][columns]);
 
 void print_matrix(int rows, int columns, double matrix[][columns], int type);
-int populate_matrix(int rows, int columns, double matrix[][columns]);
+int populate_matrix(int rows, int columns, double matrix[][columns], int type);
 int matrix_dimensions(int *rows, int *columns, int type);
 
 int gauss_jordan(void);
-void check_gauss(int rows, int columns, double matrix[][columns]);
-void check_jordan(int rows, int columns, double matrix[][columns]);
+void check_gauss(int rows, int columns, double matrix[][columns], int type, double *det_address);
+void check_jordan(int rows, int columns, double matrix[][columns], int type);
 int add_matrix(void);
 int scalar_multi(void);
 int multi_matrix(void);
 int transpose(void);
+int determinant(void);
+int inverse(void);
 
 struct operation {
     int id;
@@ -31,8 +34,6 @@ struct operation op[OPERATIONS];
 
 int main (void) 
 {
-    printf("WELCOME TO KANE's EXPERIMENTAL GENERAL PURPOSE MATRIX CALCULATOR v1.1\n\n");
-
     //name the operations
     struct operation op[OPERATIONS];
     op[0].name = "Gauss-Jordan";
@@ -40,56 +41,75 @@ int main (void)
     op[2].name = "Scalar-Multi";
     op[3].name = "Matrix-Multi";
     op[4].name = "Transpose";
+    op[5].name = "Determinant";
+    op[6].name = "Inverse";
     int selected = 0;
     int return_value = 0;
 
-    printf("Matrix Operations: \n");
+    while (1) {
+        system("cls");
+        printf("WELCOME TO KANE's EXPERIMENTAL GENERAL PURPOSE MATRIX CALCULATOR \033[33mv1.2\033[0m\n\n");
+        printf("Matrix Operations: \n");
 
-    //assign operations with ids and print ids with corresponding name
-    for(int i = 0; i < OPERATIONS; i++)
-    {
-        op[i].id = i + 1;
-        printf("%i. %s\n", op[i].id, op[i].name);
+        //assign operations with ids and print ids with corresponding name
+        for(int i = 0; i < OPERATIONS; i++)
+        {
+            op[i].id = i + 1;
+            printf("%i. %s\n", op[i].id, op[i].name);
+        }
+
+        printf("\nSelect Number (0 to exit): ");
+        scanf("%i", &selected);
+        if (selected == 0) {
+            system("cls");
+            printf("Exiting Program...");
+            getchar();
+            getch();
+        }
+
+        //if selected is invalid, exit
+        if(selected < 0 || selected > OPERATIONS) {
+            printf("Invalid Input...");
+            getch();
+            continue;
+        }
+
+        system("cls");
+
+        //select operations
+        printf("%s SELECTED\n\n", op[selected - 1].name);
+        switch(selected)
+        {
+            default:
+                return 1;
+            case 1:
+                return_value = gauss_jordan();
+                break;
+            case 2:
+                return_value = add_matrix();
+                break;
+            case 3:
+                return_value = scalar_multi();
+                break;
+            case 4:
+                return_value = multi_matrix();
+                break;
+            case 5:
+                return_value = transpose();
+                break;
+            case 6:
+                return_value = determinant();
+                break;
+            case 7:
+                return_value = inverse();
+                break;
+        }
+
+        printf("\nReturn Value = %d\nPress Enter to Continue...", return_value);
+        getchar();
+        getchar();
+        continue;
     }
-
-    printf("\nSelect Number: ");
-    scanf("%i", &selected);
-
-    //if selected is invalid, exit
-    if(selected < 0 || selected > OPERATIONS) {
-        return 1;
-    }
-
-    system("cls");
-
-    //select operations, set main's return value to it's return value
-    printf("%s SELECTED\n\n", op[selected - 1].name);
-    switch(selected)
-    {
-        case 0:
-            return 1;
-            break;
-        case 1:
-            return_value = gauss_jordan();
-            break;
-        case 2:
-            return_value = add_matrix();
-            break;
-        case 3:
-            return_value = scalar_multi();
-            break;
-        case 4:
-            return_value = multi_matrix();
-            break;
-        case 5:
-            return_value = transpose();
-            break;
-    }
-
-    printf("\nPress enter to exit...");
-    getchar();
-    getchar();
-    return return_value;
 }
 
 int gauss_jordan(void)
@@ -104,7 +124,7 @@ int gauss_jordan(void)
     double matrix[rows][columns];
     memset(matrix, 0, sizeof(matrix));
 
-    if(populate_matrix(rows, columns, matrix) != 0)
+    if(populate_matrix(rows, columns, matrix, type) != 0)
     {
         return 3;
     }
@@ -112,8 +132,8 @@ int gauss_jordan(void)
     printf("Matrix:");
     print_matrix(rows, columns, matrix, type);
 
-    check_gauss(rows, columns, matrix);
-    check_jordan(rows, columns, matrix);
+    check_gauss(rows, columns, matrix, type, NULL);
+    check_jordan(rows, columns, matrix, type);
 
     return 0;
 }
@@ -133,13 +153,13 @@ int add_matrix(void)
     memset(matrix2, 0, sizeof(matrix2));
 
     printf("1st Matrix:\n");
-    if(populate_matrix(rows, columns, matrix) != 0)
+    if(populate_matrix(rows, columns, matrix, type) != 0)
     {
         return 3;
     }
 
     printf("2nd Matrix:\n");
-    if(populate_matrix(rows, columns, matrix2) != 0)
+    if(populate_matrix(rows, columns, matrix2, type) != 0)
     {
         return 3;
     }
@@ -178,7 +198,7 @@ int scalar_multi(void)
     scanf("%lf", &factor);
     printf("\n");
 
-    if(populate_matrix(rows, columns, matrix) != 0)
+    if(populate_matrix(rows, columns, matrix, type) != 0)
     {
         return 3;
     }
@@ -191,7 +211,7 @@ int scalar_multi(void)
     {
         multi(columns, i, factor, matrix, type);
     }
-    printf("x%.2lf\n", factor);
+    printf("x\033[33m%.2lf\033[0m\n", factor);
 
     print_matrix(rows, columns, matrix, type);
 
@@ -228,13 +248,13 @@ int multi_matrix(void)
     memset(matrix3, 0, sizeof(matrix3));
 
     printf("1st Matrix:\n");
-    if(populate_matrix(rows, columns, matrix) != 0)
+    if(populate_matrix(rows, columns, matrix, type) != 0)
     {
         return 3;
     }
 
     printf("2nd Matrix:\n");
-    if(populate_matrix(rows2, columns2, matrix2) != 0)
+    if(populate_matrix(rows2, columns2, matrix2, type) != 0)
     {
         return 3;
     }
@@ -258,7 +278,7 @@ int multi_matrix(void)
                 b = b + a;
                 if(k < columns)
                 {
-                    printf(" (%.1lf x %.1lf) ", matrix[i][k], matrix2[k][j]);
+                    printf(" (\033[33m%.1lf\033[0m x \033[33m%.1lf\033[0m) ", matrix[i][k], matrix2[k][j]);
                 }
                 if(k < columns - 1)
                 {
@@ -290,7 +310,7 @@ int transpose(void)
     double matrix2[columns][rows];
     memset(matrix2, 0, sizeof(matrix2));
 
-    if(populate_matrix(rows, columns, matrix) != 0)
+    if(populate_matrix(rows, columns, matrix, type) != 0)
     {
         return 3;
     }
@@ -312,6 +332,84 @@ int transpose(void)
     return 0;
 }
 
+int determinant(void)
+{
+    int rows = 0, columns = 0, type = 5;
+
+    if(matrix_dimensions(&rows, &columns, type) != 0)
+    {
+        return 2;
+    }
+
+    double matrix[rows][columns];
+    memset(matrix, 0, sizeof(matrix));
+
+    if(populate_matrix(rows, columns, matrix, type) != 0)
+    {
+        return 3;
+    }
+
+    printf("Matrix:");
+    print_matrix(rows, columns, matrix, type);
+
+    double det = 1;
+    double *det_address = &det;
+
+    check_gauss(rows, columns, matrix, type, det_address);
+
+    for (int i = 1; i < rows; i++)
+    {
+        if (matrix[i][i] == 0) {
+            det = 0;
+            break;
+        }
+    }
+
+    printf("\nThe determinant of the matrix is %.2lf\n", det);
+    return 0;
+}
+
+int inverse(void)
+{
+    int rows = 0, columns = 0, type = 6;
+
+    if(matrix_dimensions(&rows, &columns, type) != 0)
+    {
+        return 2;
+    }
+
+    columns *= 2;
+
+    double matrix[rows][columns];
+    memset(matrix, 0, sizeof(matrix));
+
+    if(populate_matrix(rows, columns, matrix, type) != 0)
+    {
+        return 3;
+    }
+
+    for (int i = 0; i < rows; i++) {
+        matrix[i][i + (columns / 2)] = 1;
+    }
+
+    printf("Matrix:");
+    print_matrix(rows, columns, matrix, type);
+
+    check_gauss(rows, columns, matrix, type, NULL);
+
+    for (int i = 1; i < rows; i++)
+    {
+        if (matrix[i][i] == 0) {
+            printf("\n\033[031mInverse does not exist\033[0m\n");
+            return 0;
+        }
+    }
+    
+    check_jordan(rows, columns, matrix, type);
+
+    return 0;
+}
+
 int matrix_dimensions(int *rows, int *columns, int type)
 {
     while(*rows < 2 || *columns < 1 || *rows > MATRIX_LIMIT || *columns > MATRIX_LIMIT) 
@@ -324,7 +422,20 @@ int matrix_dimensions(int *rows, int *columns, int type)
                 printf("\n");
                 break;
             } else {
-                printf("Invalid input!");
+                printf("Invalid input...");\
+                getch();
+                return 2;
+            }
+        } else if (type == 5 || type == 6) { // requires square matrix
+            printf("Enter Matrix Size (int): ");
+            if(scanf("%i", rows) == 1 && *rows >= 2)
+            {
+                *columns = *rows;
+                printf("\n");
+                break;
+            } else {
+                printf("Invalid input...");
+                getch();
                 return 2;
             }
         } else {
@@ -334,7 +445,8 @@ int matrix_dimensions(int *rows, int *columns, int type)
                 printf("\n");
                 break;
             } else {
-                printf("Invalid input!");
+                printf("Invalid input...");
+                getch();
                 return 2;
             }
         }
@@ -342,15 +454,20 @@ int matrix_dimensions(int *rows, int *columns, int type)
     return 0;
 }
 
-int populate_matrix(int rows, int columns, double matrix[][columns])
+int populate_matrix(int rows, int columns, double matrix[][columns], int type)
 {
+    if (type == 6) {
+        columns /= 2;
+    }
+
     for(int x = 0; x < rows; x++) 
     {
         for (int y = 0; y < columns; y++) 
         {
-            printf("Populate Matrix, Enter Value for Matrix Index[%i][%i]: ", x, y);
+            printf("Populate Matrix, Enter Value for Matrix Index[\033[33m%i\033[0m][\033[33m%i\033[0m]: ", x, y);
             if(scanf("%lf", &matrix[x][y]) != 1) {
-                printf("Invalid Input!");
+                printf("Invalid Input...");
+                getch();
                 return 3;
             }
         }
@@ -380,6 +497,11 @@ void print_matrix(int rows, int columns, double matrix[][columns], int type)
                 printf("| ");
             }
 
+            if (y == columns / 3 && type == 6)
+            {
+                printf("|");
+            }
+
         }
             printf("\n");
     }
@@ -393,7 +515,7 @@ void add(int columns, int target_r, int source_r, double factor, double matrix[]
         matrix[target_r][j] = matrix[target_r][j] + (matrix[source_r][j] * factor);
     }
 
-    printf("\nR%i + (%.2lf)R%i\n", target_r + 1, factor, source_r + 1);
+    printf("\nR%i + (\033[33m%.2lf\033[0m)R%i\n", target_r + 1, factor, source_r + 1);
 
 }
 
@@ -404,9 +526,9 @@ void multi(int columns, int row, double factor, double matrix[][columns], int ty
         matrix[row][j] = matrix[row][j] * factor;
     }
 
-    if(type == 0)
+    if(type == 0 || type == 5 || type == 6)
     {
-    printf("\n(%.2lf)R%i\n", factor, row + 1);
+    printf("\n(\033[33m%.2lf\033[0m)R%i\n", factor, row + 1);
     }
     
 }
@@ -427,11 +549,10 @@ void swap(int columns, int row1, int row2, double matrix[][columns])
 
 }
 
-void check_gauss(int rows, int columns, double matrix[][columns]) 
+void check_gauss(int rows, int columns, double matrix[][columns], int type, double *det_address) 
 {
 
     int diag_col = 0;
-    int type = 0;
 
     for(int x = 0; x < rows; x++) 
     {
@@ -442,6 +563,9 @@ void check_gauss(int rows, int columns, double matrix[][columns])
             {
                 if(matrix[i][diag_col] != 0)
                 {
+                    if (type == 5) {
+                        *det_address *= -1;
+                    }
                     swap(columns, x, i, matrix);
                     print_matrix(rows, columns, matrix, type);
                     break;
@@ -458,7 +582,10 @@ void check_gauss(int rows, int columns, double matrix[][columns])
         //turn pivot in this row into 1
         if(matrix[x][diag_col] != 1)
         {
-            //find inverse of diagonal, then  perform operation
+            //find inverse of diagonal, then perform operation
+            if (type == 5) {
+                *det_address *= matrix[x][diag_col];
+            }
             double factor = 1.0 / matrix[x][diag_col];
             multi(columns, x, factor, matrix, type);
             print_matrix(rows, columns, matrix, type);
@@ -480,11 +607,14 @@ void check_gauss(int rows, int columns, double matrix[][columns])
     }
 }
 
-void check_jordan(int rows, int columns, double matrix[][columns]) 
+void check_jordan(int rows, int columns, double matrix[][columns], int type) 
 {
-    int type = 0;
     for (int diag_col = 0; diag_col < columns - 1; diag_col++) 
     {
+        if (type == 6 && diag_col == (columns / 2)) {
+            return;
+        }
+
         // clear out column above pivot
         for (int i = 0; i < diag_col; i++) 
         {
